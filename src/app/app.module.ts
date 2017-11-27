da1f17af0ed0ec34638debb1d6c44a7d0a6bb69d
valeriajara <valeria.jara@alumnos.upla.cl>
2017-11-27 09:32:53 -0300

    commit inicial
    20/11/2017

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,32 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+
 import { HomePage } from '../pages/home/home';
+import { MathJaxDirective } from '../directives/math-jax/math-jax';
+import { BernoulliPage } from '../pages/bernoulli/bernoulli';
+import { BinomialPage } from '../pages/binomial/binomial';
+import { ChiPage } from '../pages/chi/chi';
+import { ExponencialPage } from '../pages/exponencial/exponencial';
+import { HipergeometricaPage } from '../pages/hipergeometrica/hipergeometrica';
+import { NormalPage } from '../pages/normal/normal';
+import { PoissonPage } from '../pages/poisson/poisson';
+import { TstudentPage } from '../pages/tstudent/tstudent';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    BernoulliPage,
+    BinomialPage,
+    ChiPage,
+    ExponencialPage,
+    HipergeometricaPage,
+    NormalPage,
+    PoissonPage,
+    TstudentPage,
+    MathJaxDirective
   ],
   imports: [
     BrowserModule,
@@ -19,12 +39,20 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    BernoulliPage,
+    BinomialPage,
+    ChiPage,
+    ExponencialPage,
+    HipergeometricaPage,
+    NormalPage,
+    PoissonPage,
+    TstudentPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    ]
 })
 export class AppModule {}
